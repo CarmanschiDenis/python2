5230e6828fb6ae41ee3adfc3ccff2b2c1905148c
CarmanschiDenis <carmanschidenis@gmail.com>
2023-09-17 10:52:03 +0300

    main

---
+++ main.py
@@ -0,0 +1,98 @@
+import random
+
+#var = -2 #int
+#a = 3.14 #float
+#string = 'my text' #string
+#boolean = True #sau false = 0
+
+#print(string)
+#print(a)
+#print(boolean)
+#print(var)
+
+#print(var+a)
+#print(var-a)
+#print(var*a)
+#print(var / a)
+
+#print(boolean + var)
+#print(boolean - var)
+#print(boolean * var)
+#print(boolean / var)
+
+
+#print(4/2)
+#print(4/3)
+
+
+#value_int = int(input("Enter a int \n")) #int
+#value_float = float(input("Enter a float \n")) #float
+#value_string = input("Enter a text \n") #string
+#value_boolean = bool(input("Enter a boolean \n")) #boolean
+
+#print(value_int)
+#print(value_float)
+#print(value_string)
+#print(value_boolean)
+
+
+
+
+
+#transformare:
+#str_var = 4
+#print(int(str_var))...
+
+
+
+
+#major = 18
+#var_input = int(input("Enter you age"))
+
+#if major < var_input :
+    #print(" < ")
+#elif major == var_input:
+    #print(" = ")
+#else:
+   # print (" > ")
+
+
+#var1 = "string"
+#var2 = input("Enter a value \n")
+
+#if var1 == var2:
+  #  print("Same value")
+#else:
+ #   print("Are different")
+
+
+
+#for count in range(-10, 10):
+    #print(count) #Auto Increment
+
+
+
+#var = 1
+#while var < 100:
+    #print(var) #99
+   # var = var + 1 #99 + 1 #Manual increment
+
+
+
+
+import random
+var_check = random.randint(0, 50)
+var_in = int(input("Enter a number between 1 and 50 \n"))
+
+
+while var_in != var_check:
+    if var_in < var_check:
+        print("Less than you need")
+    else:
+        print("More than you need")
+else:
+    print("congratulations")
+
+
+
+
